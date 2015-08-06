ae10832624a213b7b5c0951c6685490e40cf40c8
Christian Witting <witting.chr@gmail.com>
2015-08-06 13:40:00 +1000

    Test code

--- robot.m
+++ robot.m
@@ -1,3 +1,8 @@
-foo = 2;
-bar = 2;
-foo + bar
+h = COM_OpenNXT()
+COM_SetDefaultNXT(h)
+mA = NXTMotor('A', 'Power', 50);
+mA.TachoLimit = 20;
+mA.SendToNXT(); % this is actually the moment we start the motor
+pause(1);
+mA.Stop('Brake');
+COM_CloseNXT(COM_GetDefaultNXT());
