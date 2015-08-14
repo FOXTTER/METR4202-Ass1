f9013f88e461e6442c44871c0708565ae08fa602
Christian Witting <witting.chr@gmail.com>
2015-08-14 10:30:53 +1000

    Added comments

--- calcAngles.m
+++ calcAngles.m
@@ -2,7 +2,7 @@ function [ alpha, beta, gamma ] = calcAngles(start, desired)
 %calcAngles Summary of this function goes here
 % Lego unit conversion 4 dots = one big square
 LEGO = 0.032;
-%Motor parameters
+%Motor gearing
 mA_gear = 55/24;
 mB_gear = 1;
 mC_gear = 1;
@@ -11,7 +11,7 @@ base = [5.5, -2.125, 0];
 % Position of joint A
 joint_A = [5.5, -2.125, 0.16/LEGO];
 %Lengths of robot
-a2b = 0.17/LEGO;
+a2b = 0.172/LEGO;
 b2c = 0.182/LEGO;
 a2c_new = norm(desired - joint_A);
 a2c_old = norm(start - joint_A);
@@ -22,9 +22,9 @@ alpha = int32(rad2deg(atan2(desired(1)-base(1),desired(2)-base(2)) - atan2(start
 % Beta = second joint
 phi_e = @(T) acos((a2b^2+T^2-b2c^2)/(2*a2b*T));
 phi_f = @(T,z_p) acos((base2a-z_p)/T);
-beta  = int32(rad2deg((phi_e(a2c_new) + phi_f(a2c_new,desired(3))) - (phi_e(a2c_old) + phi_f(a2c_old,start(3)))));
+beta  = int32(rad2deg((phi_e(a2c_new) + phi_f(a2c_new,desired(3))) - (phi_e(a2c_old) + phi_f(a2c_old,start(3))))*mB_gear);
 % Gamma = third joint
-phi_d = @(T) acos((base2a^2 + a2b^2 - T^2)/(2*base2a*a2b))
-gamma = int32(rad2deg(phi_d(a2c_new)-phi_d(a2c_old)));
+phi_d = @(T) acos((base2a^2 + a2b^2 - T^2)/(2*base2a*a2b));
+gamma = int32(rad2deg(phi_d(a2c_new)-phi_d(a2c_old))*mC_gear);
 end
 
