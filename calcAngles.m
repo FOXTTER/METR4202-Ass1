347e357f95c0466af8135401460c518207a3f3a5
Christian Witting <witting.chr@gmail.com>
2015-08-14 10:08:17 +1000

    Added angle calculation

---
+++ calcAngles.m
@@ -0,0 +1,27 @@
+function [ alpha, beta, gamma ] = calcAngles(start, desired)
+%calcAngles Summary of this function goes here
+% Lego unit conversion 4 dots = one big square
+LEGO = 0.032;
+%Motor parameters
+mA_gear = 55/24;
+mB_gear = 1;
+mC_gear = 1;
+% Position parameters of base
+base = [5.5, -2.125, 0];
+% Position of joint A
+joint_A = [5.5, -2.125, 0.16/LEGO];
+%Lengths of robot
+a2b = 0.17/LEGO;
+b2c = 0.182/LEGO;
+a2c_new = norm(desired - joint_A);
+a2c_old = norm(start - joint_A);
+base2a = 0.16/LEGO;
+% Calculations
+alpha = int32(rad2deg(atan2(desired(1)-base(1),desired(2)-base(2)) - atan2(start(1)-base(1),start(2)-base(2)))*mA_gear);
+gamma = int32(rad2deg(acos((base2a^2 + a2b^2 + a2c_new^2)/(2*base2a*a2b)) - acos((base2a^2 + a2b^2 + a2c_old^2)/(2*base2a*a2b))));
+phi_e = @(T) acos((a2b^2+T^2-b2c^2)/(2*a2b*T));
+phi_f = @(T,zp) acos((base2a-z_p)/T);
+beta  = int32(rad2deg((phi_e(a2c_new) + phi_f(a2c_new,desired(3))) - (phi_e(a2c_old) + phi_f(a2c_old,start(3)))));
+
+end
+
