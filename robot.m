cbe70844092a12dab742dc2b4f3958aa012eb4f5
Christian Witting <witting.chr@gmail.com>
2015-08-16 17:02:51 +1000

    Updated version

--- robot.m
+++ robot.m
@@ -1,11 +1,19 @@
 clc
+clear
 [h, mA, mB, mC] = setupNXT();
+mA.SpeedRegulation = true;
+mB.SpeedRegulation = true;
+mC.SpeedRegulation = true;
 disp('Put robot arm in desired position and press enter')
 pause
 mA.Stop('Brake');
 mB.Stop('Brake');
 mC.Stop('Brake');
+disp('Press enter to start')
+pause
 M = importdata('coords.txt');
+M(:,1:2) = M(:,1:2)*0.032;
+M(:,3) = M(:,3)*0.020;
 current = M(1,:,:);
 for i = 2:size(M,1)
     fprintf('Point: %d\n',i);
@@ -14,8 +22,8 @@ for i = 2:size(M,1)
     %Convert the point to angles for the motors
     [alpha, beta, gamma] = calcAngles(current, desired);
     current = desired;
-    moveEngine(mA,10,alpha);
-    moveEngine(mC,-5,gamma);
-    moveEngine(mB,-5,beta);
+    moveEngine(mA,-10,alpha);
+    moveEngine(mC,-10,gamma);
+    moveEngine(mB,-10,beta);
     pause(0.5);
 end
