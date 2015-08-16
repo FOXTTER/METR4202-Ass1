a87018d2b7b6efc1ea3c5414991f21505cbcd82e
Christian Witting <witting.chr@gmail.com>
2015-08-16 19:28:45 +1000

    Moves engines simultaneous

--- robot.m
+++ robot.m
@@ -16,14 +16,23 @@ M(:,1:2) = M(:,1:2)*0.032;
 M(:,3) = M(:,3)*0.020;
 current = M(1,:,:);
 for i = 2:size(M,1)
+    %moveEngine(mB,-10,20);
     fprintf('Point: %d\n',i);
     disp(M(i,:,:))
     desired = M(i,:,:);
     %Convert the point to angles for the motors
     [alpha, beta, gamma] = calcAngles(current, desired);
+    fprintf('Angles (a,b,g) = (%d, %d, %d)\n',alpha,beta,gamma);
     current = desired;
     moveEngine(mA,-10,alpha);
+    if (beta < 20)
+       moveEngine(mB,-2,beta);
+    else
+       moveEngine(mB,-10,beta);
+    end
     moveEngine(mC,-10,gamma);
-    moveEngine(mB,-10,beta);
+    mA.WaitFor();
+    mB.WaitFor();
+    mC.WaitFor();
     pause(0.5);
 end
