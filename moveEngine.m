e7d589e1fd4d4add45054a63e7d556e4d201b5d5
Christian Witting <witting.chr@gmail.com>
2015-08-16 19:29:19 +1000

    added calibrations

--- moveEngine.m
+++ moveEngine.m
@@ -2,13 +2,29 @@ function [ ] = moveEngine( engine, power, angle )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 % Motor B
-if (angle <= 1)
+if (abs(angle) <= 1)
     return;
+end
+port = engine.Port;
+if (port ==0)
+    %Bla
+elseif (port == 1)
+    if(angle > 0)
+        angle = angle + 4;
+    else
+        angle = angle + 6;
+    end
+elseif (port == 2)
+    if(angle > 0)
+        angle = angle + 2;
+    else
+        angle = angle + 2;
+    end
 end
 engine.TachoLimit = abs(angle); % Angle for motor A
 engine.Power = power * sign(angle);
 engine.ActionAtTachoLimit = 'Holdbrake';
 engine.SendToNXT();
-engine.WaitFor();
+%engine.WaitFor();
 end
 
