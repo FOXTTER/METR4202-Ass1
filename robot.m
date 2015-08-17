da41f4f83c3839f412754e227f89afed1eb07588
Christian Witting <witting.chr@gmail.com>
2015-08-17 12:58:39 +1000

    Lifting B before doing anything

--- robot.m
+++ robot.m
@@ -1,20 +1,23 @@
-% clc
-% clear
-% [h, mA, mB, mC] = setupNXT();
-% mA.SpeedRegulation = true;
-% mB.SpeedRegulation = true;
-% mC.SpeedRegulation = true;
-% disp('Put robot arm in desired position and press enter')
-% pause
-% mA.Stop('Brake');
-% mB.Stop('Brake');
-% mC.Stop('Brake');
-% disp('Press enter to start')
-% pause
+clc
+clear
+[h, mA, mB, mC] = setupNXT();
+mA.SpeedRegulation = true;
+mB.SpeedRegulation = true;
+mC.SpeedRegulation = true;
+disp('Put robot arm in desired position and press enter')
+pause
+mA.Stop('Brake');
+mB.Stop('Brake');
+mC.Stop('Brake');
+disp('Press enter to start')
+pause
 M = importdata('coords.txt');
 M(:,1:2) = M(:,1:2)*0.032;
 M(:,3) = M(:,3)*0.020;
 current = M(1,:,:);
+alpha_error = 0;
+beta_error = 0;
+gamma_error = 0;
 for i = 2:size(M,1)
     %moveEngine(mB,-10,20);
     fprintf('Point: %d\n',i);
@@ -24,15 +27,12 @@ for i = 2:size(M,1)
     [alpha, beta, gamma] = calcAngles(current, desired);
     fprintf('Angles (a,b,g) = (%d, %d, %d)\n',alpha,beta,gamma);
     current = desired;
-    moveEngine(mA,-50,alpha);
-    if (beta < 20)
-       moveEngine(mB,-20,beta);
-    else
-       moveEngine(mB,-20,beta);
-    end
-    moveEngine(mC,-20,gamma);
+    beta_error = moveEngine(mB,-20,30 + beta_error);
+    alpha_error = moveEngine(mA,-90,alpha + alpha_error);
+    gamma_error = moveEngine(mC,-20,gamma + gamma_error);
+    beta_error = moveEngine(mB,-20,beta - 30 + beta_error);
     mA.WaitFor();
     mB.WaitFor();
     mC.WaitFor();
-    pause(0.5);
+    pause(0.1);
 end
