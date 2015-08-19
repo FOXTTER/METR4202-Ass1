5c098223223b927d9e9a3d0fadca155f51aebfc3
Christian Witting <witting.chr@gmail.com>
2015-08-19 23:50:36 +1000

    Non working angle calc

--- robot.m
+++ robot.m
@@ -1,14 +1,14 @@
 clc
 clear
 [h] = setupNXT();
-mA = NXTMotor('A', 'Power', 30);
-mB = NXTMotor('B', 'Power', 20);
-mC = NXTMotor('C', 'Power', 10);
+mA = NXTMotor('A', 'Power', -50);
+mB = NXTMotor('B', 'Power', -15);
+mC = NXTMotor('C', 'Power', -30);
 mA.SpeedRegulation = true;
 mB.SpeedRegulation = true;
 mC.SpeedRegulation = true;
 M = importdata('coords.txt');
-M = Lab1RandLocations(6)
+%M = Lab1RandLocations(6)
 disp('Put robot arm in desired position and press enter')
 pause
 mA.Stop('Brake');
@@ -16,7 +16,6 @@ mB.Stop('Brake');
 mC.Stop('Brake');
 disp('Press enter to start')
 pause
-%%
 M(:,1:2) = M(:,1:2)*0.032;
 M(:,3) = M(:,3)*0.020;
 %current = M(1,:,:);
@@ -26,13 +25,18 @@ beta_error = 0;
 gamma_error = 0;
 alpha_old = 0;
 securityAngle = 15;
-securityInterval = 7;
 enginePowerA = -50;
 enginePowerB = -15;
 enginePowerC = -30;
 optimal = tsp_dp1(M)
 %for i = 2:size(M,1)
 i = 1;
+base2a = 0.162;
+joint_A = [5.5*0.032, -2.125*0.032, base2a];
+T_0 = norm(joint_A - [1*0.032, 0, 0]);
+beta_0 = int32(rad2deg(acos(base2a/T_0)));
+mB.ResetPosition();
+%%
 while(1)
     %moveEngine(mB,-10,20);
     fprintf('Point: %d\n',i);
@@ -41,17 +45,13 @@ while(1)
     %Convert the point to angles for the motors
     [alpha, beta, gamma] = calcAngles(current, desired);
     fprintf('Angles (a,b,g) = (%d, %d, %d)\n',alpha,beta,gamma);
-    if beta > ((securityAngle-securityInterval)*(24/8))
-        beta_error = (moveEngine(mB,enginePowerB,-securityAngle*24/8 + beta_error, alpha_old))/(24/8);
-        alpha_error = moveEngine(mA,enginePowerA,alpha + alpha_error, alpha_old);
-        gamma_error = moveEngine(mC,enginePowerC,gamma + gamma_error, alpha_old);
-        beta_error = (moveEngine(mB,enginePowerB, securityAngle*24/8 + beta + beta_error, alpha_old))/(24/8);
-    else
-        beta_error = (moveEngine(mB,enginePowerB,-securityAngle*24/8 + beta + beta_error, alpha_old))/(24/8);
-        alpha_error = moveEngine(mA,enginePowerA,alpha + alpha_error, alpha_old);
-        gamma_error = moveEngine(mC,enginePowerC,gamma + gamma_error, alpha_old);
-        beta_error = (moveEngine(mB,enginePowerB, securityAngle*24/8 + beta_error, alpha_old))/(24/8);
-    end
+    %securityAngle = int32(rad2deg(asin((0.10 - base2a)/(norm(current - joint_A)))) - mB.ReadFromNXT().Position);
+    securityAngle = int32((90-rad2deg(asin((base2a - 0.1)/(norm(current - joint_A)))) - ((mB.ReadFromNXT().Position/3) + beta_0)));
+    pause
+    beta_error = (moveEngine(mB,enginePowerB,securityAngle*24/8 + beta_error, alpha_old))/(24/8);
+    alpha_error = moveEngine(mA,enginePowerA,alpha + alpha_error, alpha_old);
+    gamma_error = moveEngine(mC,enginePowerC,gamma + gamma_error, alpha_old);
+    beta_error = (moveEngine(mB,enginePowerB, -securityAngle*24/8 + beta + beta_error, alpha_old))/(24/8);
 %     mA.WaitFor(); % is contained in moveEngine
 %     mB.WaitFor();
 %     mC.WaitFor();
